9d659cd450c98ae35a8bf4d47bf57054445d032b
ubertakter <ubertakter@users.noreply.github.com>
2017-12-13 17:21:55 -0600

    Update DataModel.java

--- src/combinedeventstreamtest/DataModel.java
+++ src/combinedeventstreamtest/DataModel.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package combinedeventstreamtest;
 
 import java.util.Objects;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
-/**
- *
- * @author bwhitworth
- */
 public class DataModel {
    private DoubleProperty a, b;
    
