17ca6009d6cc7deb8d89d4b1286f6cde1a754c0f
ubertakter <ubertakter@users.noreply.github.com>
2017-12-13 17:15:11 -0600

    Update ChangeA.java

--- src/combinedeventstreamtest/ChangeA.java
+++ src/combinedeventstreamtest/ChangeA.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package combinedeventstreamtest;
 
 import java.util.Optional;
 import org.reactfx.Change;
 
-/**
- *
- * @author bwhitworth
- */
 public class ChangeA extends ChangeBase<Double> {
 
    public ChangeA(DataModel model, double oldVal, double newVal) {
