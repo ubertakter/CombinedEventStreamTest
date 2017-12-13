d698cd6d6d8c3ab5dae87bf81da65508cc74f686
ubertakter <ubertakter@users.noreply.github.com>
2017-12-13 17:20:58 -0600

    Update ChangeB.java

--- src/combinedeventstreamtest/ChangeB.java
+++ src/combinedeventstreamtest/ChangeB.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package combinedeventstreamtest;
 
 import java.util.Optional;
 import org.reactfx.Change;
 
-/**
- *
- * @author bwhitworth
- */
 public class ChangeB extends ChangeBase<Double> {
 
    public ChangeB(DataModel model, double oldVal, double newVal) {
