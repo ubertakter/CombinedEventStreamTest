e7d5e6438d6889a95da32aa10595e41c2143e181
ubertakter <ubertakter@users.noreply.github.com>
2017-12-13 17:13:00 -0600

    Update ChangeBase.java

--- src/combinedeventstreamtest/ChangeBase.java
+++ src/combinedeventstreamtest/ChangeBase.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package combinedeventstreamtest;
 
 import java.util.Objects;
 import java.util.Optional;
 
-/**
- *
- * @author bwhitworth
- */
 public abstract class ChangeBase<T> implements UndoChange {
    protected final T oldValue, newValue;
    protected final DataModel model;
