e7edb5c5f358dc7f7cd78e8d99a51291b2cd3371
ubertakter <ubertakter@users.noreply.github.com>
2017-12-13 17:22:16 -0600

    Update UndoChange.java

--- src/combinedeventstreamtest/UndoChange.java
+++ src/combinedeventstreamtest/UndoChange.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package combinedeventstreamtest;
 
 import java.util.Optional;
 
-/**
- *
- * @author bwhitworth
- */
 public interface UndoChange {
    public void redo();
    public UndoChange invert();
